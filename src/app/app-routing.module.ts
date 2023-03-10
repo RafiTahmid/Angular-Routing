0a189f1f323ca1abc8ae6c64299a0ddfea641cb9
RafiTahmid <rafi.tahmid019@gmail.com>
2023-03-10 23:45:29 +0600

    setup basic routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { UserComponent } from './user/user.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    component: HomeComponent,
+    path: ''
+  },
+  {
+    component: AboutComponent,
+    path: 'about'
+  },
+  {
+    component: UserComponent,
+    path: 'user'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
